293c93fee39b4a662f02b0a57b6346aa9692a297
rpuhalovich <reanpuhalovich@gmail.com>
2021-08-29 22:30:32 +1000

    Cleanup.

--- src/scene/MyStopwatch.cs
+++ src/scene/MyStopwatch.cs
@@ -22,12 +22,13 @@ namespace RayTracer
         public void Stop()
         {
             this.sw.Stop();
+
             // Get the elapsed time as a TimeSpan value.
             TimeSpan ts = this.sw.Elapsed;
 
             // Format and display the TimeSpan value.
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-            Console.WriteLine("RunTime: " + elapsedTime);
+            Console.WriteLine("Runtime: " + elapsedTime);
         }
     }
 }
