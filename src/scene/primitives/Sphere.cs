37efce31616d2e60591654f322322d31bb7541a9
rpuhalovich <reanpuhalovich@gmail.com>
2021-08-30 14:15:22 +1000

    Redid my sphere intersection code...still the same problem.

--- src/scene/primitives/Sphere.cs
+++ src/scene/primitives/Sphere.cs
@@ -26,27 +26,26 @@ namespace RayTracer
 
         /// <summary>
         /// Determine if a ray intersects with the sphere, and if so, return hit data.
+        /// From: http://kylehalladay.com/blog/tutorial/math/2013/12/24/Ray-Sphere-Intersection.html
         /// </summary>
         /// <param name="ray">Ray to check</param>
         /// <returns>Hit data (or null if no intersection)</returns>
         public RayHit Intersect(Ray ray)
         {
-            double offset = 0.0001f;
-
             Vector3 L = this.center - ray.Origin;
-            double tca = L.Dot(ray.Direction);
-            double d2 = L.Dot(L) - tca * tca; //float d2 = L*L - tca*tca;
+            double tc = L.Dot(ray.Direction.Normalized());
+            if (tc < 0.0f) return null;
+
+            double d2 = L.Dot(L) - tc * tc;
             if (d2 > this.radius * this.radius) return null;
-            double thc = Math.Sqrt(this.radius * this.radius - d2);
-            double t0 = tca - thc; // t0 is the t value for distance of ray.
-            double t1 = tca + thc;
-            if (t0 < offset) t0 = t1;
-            if (t0 < offset) return null;
 
-            Vector3 pos = ray.At(t0);
-            pos = new Vector3(pos.X, pos.Y, pos.Z * -1);
+            double t1c = Math.Sqrt((this.radius * this.radius) - d2);
+            double t1 = tc - t1c;
+            double t2 = tc + t1c;
 
-            return new RayHit(pos, pos - this.center.Normalized(), ray.Direction, this.material);
+            Vector3 pos = ray.At(t1);
+            if (ray.At(t2).LengthWith(ray.Origin) < ray.At(t1).LengthWith(ray.Origin)) pos = (ray.At(t2));
+            return new RayHit(pos, (pos - this.center).Normalized(), ray.Direction, this.material);
         }
 
         /// <summary>
