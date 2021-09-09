ca68c16b70aaaee6ab56292daa5993b8906cf5b7
rpuhalovich <reanpuhalovich@gmail.com>
2021-09-10 09:06:55 +1000

    Added .sh final-scene script.

---
+++ scripts/final_scene.sh
@@ -0,0 +1,5 @@
+cd $(dirname $0)
+cd ..
+
+python ./tests/final_scene.py
+dotnet run -- -f tests/final_scene.txt -o images/final_scene.png
--- tests/final_scene.py
+++ tests/final_scene.py
@@ -1,10 +1,26 @@
+# Written by Ryan Puhalovich 1064808
+# Made because I wanted comments, variables, arithmetic and functions.
+# USAGE: Run scripts/final_scene.sh or scripts/final_scene.bat
+
 import random
+
+# Marble Parameters
+NUM_MARBLES = 100
+MARBLE_RADIUS = 0.12 # TODO: make random
+
+# Box Parameters
+SIDE_LEN = 8.0
+FLOOR_HEIGHT = -1
+
+# Light Parameters
+LIGHT_INTENSITY = 0.5
 
 def gen():
     f = open("tests/final_scene.txt", "w")
 
     f.write('Material "WhiteMat" Diffuse (.9, .85, .9) 1\n')
     f.write('Material "PurpleMat" Diffuse (.9, 0, .9) 1\n')
+    f.write('Material "lightGreyMat" Diffuse (.9, .9, .9) 1\n')
     f.write('Material "GreyMat" Diffuse (.5, .5, .5) 1\n')
     f.write('Material "RedMat" Diffuse (1, .5, .5) 1\n')
     f.write('Material "GreenMat" Diffuse (.5, 1, .5) 1\n')
@@ -14,10 +30,10 @@ def gen():
     f.write('Material "MirrorMat" Reflective (1, 1, 1) 1\n')
     f.write('\n')
 
-    f.write('PointLight "Light1" (-1.0, 0.0, 0) (0.4, 0.4, 0.4)\n')
-    f.write('PointLight "Light2" (1, 0.0, 0) (0.4, 0.4, 0.4)\n')
-    #f.write('PointLight "Light3" (-4.0, 0.0, 10) (0.4, 0.4, 0.4)\n')
-    #f.write('PointLight "Light4" (4, 0.0, 10) (0.4, 0.4, 0.4)\n')
+    f.write('PointLight "Light1" (-1.0, 0.0, 0) (' + str(LIGHT_INTENSITY) + ', ' + str(LIGHT_INTENSITY) + ', ' + str(LIGHT_INTENSITY) + ')\n')
+    f.write('PointLight "Light2" (1, 0.0, 0) (' + str(LIGHT_INTENSITY) + ', ' + str(LIGHT_INTENSITY) + ', ' + str(LIGHT_INTENSITY) + ')\n')
+    # f.write('PointLight "Light3" (-4.0, 0.0, 10) (0.4, 0.4, 0.4)\n')
+    # f.write('PointLight "Light4" (4, 0.0, 10) (0.4, 0.4, 0.4)\n')
     f.write('\n')
 
     f.write('Sphere "BigBoi1" (-1.5, 0, 10) 1 "GreyMat"\n')
@@ -25,21 +41,19 @@ def gen():
     f.write('Sphere "BigBoi3" (1.25, 0, 5) 1 "MirrorMat"\n')
     f.write('\n')
 
-    sidelen = 10.0
-    f.write('Triangle "FloorA" (-' + str(sidelen) + ', -1, -' + str(sidelen) + ') (-' + str(sidelen) + ', -1, ' + str(sidelen) + ') (' + str(sidelen) + ', -1, ' + str(sidelen) + ') "LightBlueMat"\n')
-    f.write('Triangle "FloorB" (-' + str(sidelen) + ', -1, -' + str(sidelen) + ') (' + str(sidelen) + ', -1, ' + str(sidelen) + ') (' + str(sidelen) + ', -1, -' + str(sidelen) + ') "LightBlueMat"\n')
+    sideLenAdj = SIDE_LEN - MARBLE_RADIUS
+    f.write('Plane "Floor" (0, -1, 0) (0, 1, 0) "lightGreyMat"\n')
+    f.write('Plane "Roof" (0, ' + str(sideLenAdj) + ', 0) (0, -1, 0) "WhiteMat"\n')
+    f.write('Plane "BackWall" (0, 0, ' + str(sideLenAdj * 2) + ') (0, 0, -1) "WhiteMat"\n')
+    f.write('Plane "BehindWall" (0, 0, -' + str(sideLenAdj) + ') (0, 0, 1) "WhiteMat"\n')
+    f.write('Plane "Left" (-' + str(sideLenAdj / 2) + ', 0, 0) (1, 0, 0) "RedMat"\n')
+    f.write('Plane "Right" (' + str(sideLenAdj / 2) + ', 0, 0) (-1, 0, 0) "GreenMat"\n')
     f.write('\n')
 
-    f.write('Plane "BackWall" (0, 0, ' + str(sidelen) + ') (0, 0, -1) "WhiteMat"\n')
-    f.write('Plane "BehindWall" (0, 0, -' + str(sidelen) + ') (0, 0, 1) "WhiteMat"\n')
-    f.write('Plane "Roof" (0, ' + str(sidelen) + ', 0) (0, -1, 0) "WhiteMat"\n')
-    f.write('Plane "Left" (-' + str(sidelen) + ', 0, 0) (1, 0, 0) "RedMat"\n')
-    f.write('Plane "Right" (' + str(sidelen) + ', 0, 0) (-1, 0, 0) "GreenMat"\n')
-    f.write('\n')
-
-    for i in range(1, 11):
-        x = random.uniform(-sidelen, sidelen)
-        z = random.uniform(-sidelen, sidelen)
+    marbleHeight = MARBLE_RADIUS + FLOOR_HEIGHT
+    for i in range(1, NUM_MARBLES + 1):
+        x = random.uniform(-SIDE_LEN, SIDE_LEN)
+        z = random.uniform(-SIDE_LEN, SIDE_LEN)
 
         mattype = ""
         mat = random.randint(0, 2)
@@ -50,7 +64,7 @@ def gen():
         elif mat == 2:
             mattype = "MirrorMat"
 
-        f.write('Sphere "Marble' + str(i) + '" (' + str(x) + ', -0.9,' + str(z) + ') 0.1 "' + mattype + '"\n')
+        f.write('Sphere "Marble' + str(i) + '" (' + str(x) + ', ' + str(marbleHeight) + ',' + str(z) + ') ' + str(MARBLE_RADIUS) + ' "' + mattype + '"\n')
 
     f.close()
 
